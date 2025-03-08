fe459a125149866728ff4458a5dd6689d03aaa69
Rubul Islam <114800261+rubulgithub@users.noreply.github.com>
2025-03-08 20:55:14 +0530

    app.jsx

--- src/app.js
+++ src/app.js
@@ -22,8 +22,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   pingTimeout: 60000,
   cors: {
-    origin:
-      process.env.CORS_ORIGIN?.split(",").map((origin) => origin.trim()) || [],
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   },
 });
@@ -41,38 +40,39 @@ app.use(
 
 app.use(requestIp.mw());
 
-// Rate Limiter
+// Rate limiter to avoid misuse of the service and avoid cost spikes
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5000,
-  standardHeaders: true,
-  legacyHeaders: false,
-  keyGenerator: (req) => req.clientIp,
-  handler: (req, res) => {
-    res
-      .status(429)
-      .json(new ApiResponse(429, "Too many requests. Try again later.", null));
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req, res) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
   },
 });
 
+// Apply the rate limiting middleware to all requests
 app.use(limiter);
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
-// Session Middleware (Security Enhanced)
 app.use(cookieParser());
 
+// required for passport
 app.use(
   session({
     secret: process.env.EXPRESS_SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Secure in production
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Allow cross-site cookies
-    },
+    resave: true,
+    saveUninitialized: true,
   })
 );
 
