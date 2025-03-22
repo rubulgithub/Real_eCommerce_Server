095cf0f7b973b2e620a68ac34fbfa080a66361cd
Rubul Islam <114800261+rubulgithub@users.noreply.github.com>
2025-03-22 13:25:04 +0530

    dfdf

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -384,8 +384,6 @@ const handleSocialLogin = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict", // Prevent CSRF attacks
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
 
   return res
